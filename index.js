79f8d3c47ba0c0d2c6dc0df7df358a15c97e3b6a
shekharr22 <shekharjadhav501@gmail.com>
2025-01-25 21:40:56 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
-const path = require("path");
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const path = require('path');    
+const ejs = require('ejs')
+const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
 
-const userRoute = require("./routes/user");
+mongoose.connect("mongodb://localhost:27017/blogify")
+.then((e)=> console.log("MongoDB Connected"))
 
+const Blog = require('./models/blog')
+
+const userRoute = require('./routes/user')
+const blogRoute = require('./routes/blog')
+
+const { checkForAuthenticationCookie } = require('./middlewares/authentication');
 const app = express();
-const PORT = 8000;
+const PORT = 800;
 
-mongoose
-  .connect("mongodb://localhost:27017/blogify")
-  .then((e) => console.log("MongoDB Connected"));
+app.set("view engine","ejs")
+app.set("views", path.resolve( "views"));
 
-app.set("view engine", "ejs");
-app.set("views", path.resolve("./views"));
+//middleware
+app.use(express.urlencoded({extended:false}))
+app.use(cookieParser())
+app.use(checkForAuthenticationCookie("token"))
+app.use(express.static(path.resolve('./public')))
 
-app.use(express.urlencoded({ extended: false }));
+app.get('/',async(req,res)=>{
+    const allBlogs = await Blog.find({});
+    return res.render('home',{
+        user: req.user,
+        blogs: allBlogs
+    });
+})
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+app.use('/user',userRoute) 
+// If any request start with /user then use `userRoute`
+app.use('/blog',blogRoute) 
 
-app.use("/user", userRoute);
 
-app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
+
+
+app.listen(PORT , ()=>console.log(`Server started at PORT:${PORT}`));
