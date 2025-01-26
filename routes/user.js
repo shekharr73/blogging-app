d7d57507105b81f433758c97bb8c2f242ab0a7cd
shekharr22 <shekharjadhav501@gmail.com>
2025-01-26 09:52:46 +0530

    initial commit

--- routes/user.js
+++ routes/user.js
@@ -1,53 +1,39 @@
-const { Router } = require("express");
-const User = require("../models/user");
-
+const {Router} = require('express');
+const User = require('../models/user')
 const router = Router();
 
-router.get("/signin", (req, res) => {
-  return res.render("signin");
-});
-
-router.post("/signin", async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(401).send("Invalid email or password");
-    }
-
-    const isMatch = await user.matchPassword(password);
-    if (!isMatch) {
-      return res.status(401).send("Invalid email or password");
-    }
-
-    // Optionally, you can set a session or token here
-    return res.redirect("/");
-  } catch (error) {
-    console.error("Error during sign-in:", error);
-    return res.status(500).send("Internal Server Error");
-  }
-});
-
-router.get("/signup", (req, res) => {
-  return res.render("signup");
-});
-
-router.post("/signup", async (req, res) => {
-  const { fullName, email, password } = req.body;
-
-  try {
-    const user = await User.create({
-      fullName,
-      email,
-      password
+router.get('/signin',(req,res)=>{
+    return res.render("signin")
+})
+router.get('/signup',(req,res)=>{
+    return res.render("signup");
+})
+router.post('/signup',async (req,res)=>{
+    const {fullName , password , email } = req.body;
+    await User.create({
+        fullName,
+        email,
+        password
     });
+    return res.redirect('/');
+})
 
-    return res.redirect("/");
-  } catch (error) {
-    console.error("Error creating user:", error);
-    return res.status(500).send("Internal Server Error");
-  }
-});
+router.post('/signin',async(req,res)=>{
+    const { password , email  } = req.body;
+    try {
+        const token =await User.matchPasswordAndGenerateToken(email,password);
+        console.log("Token",token);
+        return res.cookie('token',token).redirect('/');
+    } catch (error) {
+        return res.render('signin',{
+            error:"Incorrect Email or Password",
+        });
+    }
+    
 
-module.exports = router;
+})
+
+router.get('/logout',(req, res)=>{
+    res.clearCookie('token').redirect('/');
+})
+module.exports=router;
