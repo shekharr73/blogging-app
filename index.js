74424ffd4ce091aae71b4592b7b5cd4ddea0637d
shekharr22 <shekharjadhav501@gmail.com>
2025-01-27 19:35:40 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const morgan = require('morgan'); // Added morgan for logging
+const morgan = require('morgan');
 const path = require('path');    
 const ejs = require('ejs')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
 
-mongoose.connect("mongodb://localhost:27017/blogify")
+mongoose.connect(process.env.MONGO_URL)
 .then((e)=> console.log("MongoDB Connected"))
 
 const Blog = require('./models/blog')
@@ -15,13 +15,12 @@ const blogRoute = require('./routes/blog')
 
 const { checkForAuthenticationCookie } = require('./middlewares/authentication');
 const app = express();
-const PORT = 8080; // Changed port to 8080
+const PORT = process.env.port || 8080;
 
 app.set("view engine","ejs")
 app.set("views", path.resolve( "views"));
 
-//middleware
-app.use(morgan('dev')); // Use morgan to log requests
+app.use(morgan('dev')); 
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser())
 app.use(checkForAuthenticationCookie("token"))
@@ -36,7 +35,6 @@ app.get('/',async(req,res)=>{
 })
 
 app.use('/user',userRoute) 
-// If any request start with /user then use `userRoute`
 app.use('/blog',blogRoute) 
 
 app.listen(PORT , ()=>console.log(`Server started at PORT:${PORT}`));
